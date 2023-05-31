c01923a5569006223aaa483957e9246b6fed610c
Jonas Jetschni <jetschni@users.noreply.github.com>
2023-05-31 23:35:25 +0200

    Update title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,9 @@ import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Tadamun - Supercharge now",
-  description: "Hire remote talent for lower costs",
+  title: "Looking for a remote job for 🇩🇪 or 🇪🇺 companies?",
+  description:
+    "Are you looking for a remote job for a great German 🇩🇪 or European 🇪🇺 company? We help individuals from Egypt to find part & full-time remote jobs in Germany and Europe.",
 };
 
 export default function RootLayout({
