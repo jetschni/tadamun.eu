8c74df64f261669b4f17f9736c0c23a466748216
Jonas Jetschni <jetschni@users.noreply.github.com>
2023-06-01 00:10:42 +0200

    Add airtable link

--- src/components/Landing/Roles/RolesCard.tsx
+++ src/components/Landing/Roles/RolesCard.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { type } from "os";
 import React from "react";
 
@@ -13,9 +14,10 @@ function RolesCard({ name, background_image, description }: RoleCardProps) {
       <div className="h-full max-w-sm lg:max-w-none mx-auto py-10 px-8 bg-white rounded-2xl">
         <h4 className="text-3xl font-medium mb-2">{name}</h4>
         <p className="max-w-2xs mb-10 pr-10">{description}</p>
-        <a
+        <Link
           className="group relative inline-block h-16 w-full bg-blueGray-900 rounded"
-          href={"mailto:hello@tadamun.xyz?subject=Interest in " + name}
+          href={"https://airtable.com/shr3YHd3xTfS0tLEv?prefill_Role=" + name}
+          target="_blank"
         >
           <div className="absolute top-0 left-0 transform -translate-y-1 -translate-x-1 w-full h-full group-hover:translate-y-0 group-hover:translate-x-0 transition duration-300">
             <div className="flex h-full w-full items-center justify-center bg-white border-2 border-blueGray-900 rounded">
@@ -24,7 +26,7 @@ function RolesCard({ name, background_image, description }: RoleCardProps) {
               </span>
             </div>
           </div>
-        </a>
+        </Link>
       </div>
     </div>
   );
